c85c6039af5b27ae4b810b986aa1684b0b737f14
DrazThan <oz.tal000@gmail.com>
2024-10-24 14:57:30 -0400

    adding VM and Disk

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,30 @@ resource "aws_security_group" "ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_instance" "centos" {
+  count         = 2
+  ami           = "ami-0df2a11dd1fe1f8e3"  # Replace with the actual AMI ID
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.ssh.name]
+  user_data     = file("user_data.sh")
+  associate_public_ip_address = true
+}
+
+resource "aws_ebs_volume" "data_volume" {
+  count             = 4  # 2 volumes per instance
+  availability_zone = element(aws_instance.centos.*.availability_zone, count.index % 2)
+  size              = 1
+}
+
+resource "aws_volume_attachment" "ebs_attachment" {
+  count       = 4
+  device_name = element(["/dev/sdf", "/dev/sdg"], count.index % 2)
+  volume_id   = aws_ebs_volume.data_volume[count.index].id
+  instance_id = aws_instance.centos[count.index / 2].id
+}
+resource "aws_eip" "elastic_ip" {
+  count    = 2
+  instance = aws_instance.centos[count.index].id
+  vpc      = true
+}
